1345d50654592de0a784e59c2ac48f749c1d8b58
dongjin_kim <75241209+shiGeumChi@users.noreply.github.com>
2022-06-10 01:27:22 +0900

    Add comments

--- downstreamRegexParsing.groovy
+++ downstreamRegexParsing.groovy
@@ -1,4 +1,11 @@
 // parsing each downstreamResults to upload the downstream link on Jenkins Dashboard
+// ex) url -> https://Xxxxx.xxxx.co.kr/ci/job/blahblah/job/qnx.upstream.downstream/163
+// this regex logic is for extracting qnx.upstream.downstream.
+
+// positive lookahead from /[0-9] ( /123 | /343 | /221 | .... | /1 )
+// starts from / (slash)
+// result : /(text_to_be_extracted)/123
+
 if (downstreamResults) {
     Logger.info("Post pipeline downstream results:")
     downstreamResults.each { url, buildData ->
